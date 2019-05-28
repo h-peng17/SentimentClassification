53837f34bec8857a4a74168e2da41beb6bc281d5
PH <penghao20170136@163.com>
2019-05-29 00:22:47 +0800

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -10,9 +10,9 @@ class CNN(nn.Module):
         self.in_channels = config.embedding_size
         self.out_channels = config.hidden_size
         self.kernel_size = config.kernel_size
-        self.cnn1 = nn.Conv1d(self.in_channels, self.out_channels, self.kernel_size, padding = int((self.kernel_size-1) / 2))
-        self.cnn2 = nn.Conv1d(config.hidden_size, config.hidden_size, self.kernel_size, padding = int((self.kernel_size-1) / 2))
-        self.cnn3 = nn.Conv1d(config.hidden_size, config.hidden_size, self.kernel_size, padding = int((self.kernel_size-1) / 2))
+        self.cnn1 = nn.Conv1d(self.in_channels, self.out_channels, 7, padding = int((7-1) / 2))
+        self.cnn2 = nn.Conv1d(config.hidden_size, config.hidden_size, 5, padding = int((5-1) / 2))
+        self.cnn3 = nn.Conv1d(config.hidden_size, config.hidden_size, 3, padd    ing = int((3-1) / 2))
         nn.init.xavier_normal_(self.cnn1.weight)
         nn.init.xavier_normal_(self.cnn2.weight)
         self.activation = nn.ReLU()
@@ -31,9 +31,9 @@ class CNN(nn.Module):
         # x [B, E, N] -> [B, H, N]
         x = self.cnn1(x)
         # # x [B, H, N] -> [B, H2, N]
-        # x = self.cnn2(x)
+        x = self.cnn2(x)
         # # x [B, H, N] -> [B, H2, N]
-        # x = self.cnn3(x)
+        x = self.cnn3(x)
         #x [B, H2, N] -> [B, H2]
         x = self.max_pooling(x)
         x = self.activation(x)

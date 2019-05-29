fa62179c6461717db9c87440a7b4a21972a58457
PH <penghao20170136@163.com>
2019-05-29 14:21:47 +0800

    Update gen_data.py

--- gen_data.py
+++ gen_data.py
@@ -30,8 +30,62 @@ class Gendata():
             for word in sen:
                 if word not in self.word2id:
                     self.word2id[word] = len(self.word2id)
+    
+    def process_trec_vec(self):
+        f = open('../original_data/TREC_train.txt', encoding = 'utf8')
+        self.word2id["BLK"] = 0
+        self.word2id["UNK"] = 1
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            parts = line.strip().split(":")
+            if parts[0] not in self.mood2id:
+                self.mood2id[parts[0]] = len(self.mood2id)
+            sen = parts[1].split()
+            for word in sen:
+                if word not in self.word2id:
+                    self.word2id[word] = len(self.word2id)
 
-    def process_data(self, mode, ):
+                        
+    def process_trec(self, mode):
+        f = open('../original_data/TREC_{}.txt'.format(mode))
+        total = 0
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            total += 1
+          
+        f.close()
+        f = open("../original_data/TREC_{}.txt".format(mode))
+        data_word = np.zeros(shape = [total, self.MAX], dtype = np.int32)
+        data_label = np.zeros(shape = [total], dtype = np.int32)
+        data_length = np.zeros(shape = [total], dtype = np.int32)
+        for i in range(total):
+            line = f.readline()
+            parts = line.strip().split(":")
+            sen = parts[1].split()
+            for j, word in enumerate(sen):
+                if j >= self.MAX:
+                    break
+                try:
+                    data_word[i][j] = self.word2id[word]
+                except:
+                    data_word[i][j] = self.word2id["UNK"]
+            data_length[i] = len(sen)
+            data_label[i] = self.mood2id[parts[0]]
+        
+        f = open("../data/config.json", 'w')
+        json.dump({"word_total":len(self.word2id), "mood_total": len(self.mood2id), "sen_len": self.MAX}, f)
+        f.close()
+
+        np.save("../data/{}_word.npy".format(mode), data_word)
+        np.save("../data/{}_label.npy".format(mode), data_label)
+        np.save("../data/{}_length.npy".format(mode), data_length)
+                    
+    
+    def process_data(self, mode):
         f = open("../original_data/sinanews.{}".format(mode), encoding = 'utf8')
         total = 0
         while True:
@@ -83,8 +137,15 @@ class Gendata():
 if not os.path.exists("../data"):
     os.mkdir("../data")
 
-gen = Gendata(int(sys.argv[2]))
-gen.process_vec()
-gen.process_data(sys.argv[1])
+if sys.argv[3] == 'sina':
+    print("generate sina data....")
+    gen = Gendata(int(sys.argv[2]))
+    gen.process_vec()
+    gen.process_data(sys.argv[1])
 
+elif sys.argv[3] == 'trec':
+    print("generate trec data....")
+    gen = Gendata(int(sys.argv[2]))
+    gen.process_trec_vec()
+    gen.process_trec(sys.argv[1])
 

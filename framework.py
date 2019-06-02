dded94df3f0de3eb6e5c8af5f997af832709f0b2
PH <penghao20170136@163.com>
2019-06-02 20:58:45 +0800

    Update framework.py

--- framework.py
+++ framework.py
@@ -251,8 +251,8 @@ class Test():
             # pdb.set_trace()
             for i in range(8):
                 a = pearsonr(data_dis[i].tolist(), self.test_data_loader.dis[i].tolist())
-                if a[0] > cov_max[i]:
-                    cov_max[i] = a[0]
+                if a[1] > cov_max[i]:
+                    cov_max[i] = a[1]
 
 
         print(cov_max)

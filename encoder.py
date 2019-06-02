82928ede59b929137c968ee71e580ab0f12512ec
PH <penghao20170136@163.com>
2019-06-02 14:23:25 +0800

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -10,9 +10,7 @@ class CNN(nn.Module):
         self.in_channels = config.embedding_size
         self.out_channels = config.hidden_size
         self.kernel_size = config.kernel_size
-        self.cnn1 = nn.Conv1d(self.in_channels, self.out_channels, self.kernel_size, padding = int((self.kernel_size-1) / 2))
-        self.cnn2 = nn.Conv1d(config.hidden_size, config.hidden_size, 5, padding = int((5-1) / 2))
-        self.cnn3 = nn.Conv1d(config.hidden_size, config.hidden_size, 3, padding = int((3-1) / 2))
+        self.cnn = nn.Conv1d(self.in_channels, self.out_channels, self.kernel_size, padding = int((self.kernel_size-1) / 2))
         nn.init.xavier_normal_(self.cnn1.weight)
         nn.init.xavier_normal_(self.cnn2.weight)
         self.activation = nn.ReLU()
@@ -24,24 +22,21 @@ class CNN(nn.Module):
         sen_embedding, _ = torch.max(x, dim = 2)
         return sen_embedding
     
-    def attation(self, x):
-        # x [B, N, E] -> [B, E, N]
+    def self_att(self, x):
+        # x [B, N, E]
         K = x.permute(0, 2, 1)
-        attention_x = torch.matmul((torch.matmul(x, K)/ x.size(1) ** -0.5), x)
+        # x [B, N, E]
+        attention_x = torch.matmul(self.softmax(torch.matmul(x, K)/ x.size(1) ** -0.5), x)
         return attention_x
         
     def forward(self, x):
         x = self.dropout(x)
-        # x = self.attation(x)
+        x = self.attation(x)
         # x [B, N, E] -> [B, E, N]
         x = x.permute(0, 2, 1)
         # x [B, E, N] -> [B, H, N]
-        x = self.cnn1(x)
-        # # # x [B, H, N] -> [B, H2, N]
-        # x = self.cnn2(x)
-        # # # x [B, H, N] -> [B, H2, N]
-        # x = self.cnn3(x)
-        #x [B, H2, N] -> [B, H2]
+        x = self.cnn(x)
+        #x [B, H, N] -> [B, H]
         x = self.max_pooling(x)
         x = self.activation(x)
         x = self.dropout(x)
